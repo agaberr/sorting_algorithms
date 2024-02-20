e0030450bba32943627e8a1aca2efaa595d18b6c
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2024-02-20 21:48:07 +0200

    add shell function to .h file

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,23 +1,6 @@
 #include "sort.h"
 
 /**
-* swap - swap 2 integers
-* @a: first number
-* @b: second number
-*
-* Return: None
-*/
-
-void swap(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-/**
 * shell_sort - Sort array of numbers using shell sort Algorithm
 * @array: array of numbers
 * @size: size of array
