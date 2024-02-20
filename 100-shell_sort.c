57ec5d0ddff6089e3606793720fd32a51c9c6506
SeifAlsayed02 <Seifalsayed0202@gmail.com>
2024-02-20 21:47:18 +0200

    shell sort finished

---
+++ 100-shell_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+
+/**
+* swap - swap 2 integers
+* @a: first number
+* @b: second number
+*
+* Return: None
+*/
+
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+* shell_sort - Sort array of numbers using shell sort Algorithm
+* @array: array of numbers
+* @size: size of array
+*
+* Return: None
+*/
+void shell_sort(int *array, size_t size)
+{
+
+	size_t gap = 1;
+	size_t i, j;
+	int temp;
+
+	while (gap < size / 3)
+	{
+		gap = gap * 3 + 1;
+	}
+
+	for (; gap > 0; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
